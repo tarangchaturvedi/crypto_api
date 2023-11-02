693d07113c4293d5cd2d208655a627f75490b7d2
tarangchaturvedi <tarang.chaturvedi11@gmail.com>
2023-11-02 07:47:12 +0530

    Update api_runner_func.py

--- api_runner_func.py
+++ api_runner_func.py
@@ -2,63 +2,54 @@ import requests
 import datetime
 import pandas as pd
 import json
-import time
 
-def api_runner():
-    session = requests.Session()
+API_KEY = 'YOUR_API_KEY'  # Replace with your CoinMarketCap API key
+API_URL = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+PARAMS = {
+    'start': '1',
+    'limit': '15',
+    'convert': 'USD'
+}
 
-    json_url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-    parameters = {
-        'start':'1',
-        'limit':'5000',
-        'convert':'USD'
-        }
+def get_crypto_data(api_url, params, api_key):
     headers = {
         'Accepts': 'application/json',
-        'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
-        }
+        'X-CMC_PRO_API_KEY': api_key,
+    }
 
+    try:
+        response = requests.get(api_url, headers=headers, params=params)
 
-    # Get the current timestamp
-    timestamp = datetime.datetime.now()
-    # Send an HTTP GET request to the JSON URL with the specified headers within the session
-    response = session.get(json_url, headers=headers)
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Load the JSON data
-        try:
-            data = json.loads(response.text)
-        except json.JSONDecodeError:
-            print("Failed to parse JSON response.")
-            data = None
-
-        if data:
-            # Normalize the JSON data and create a DataFrame
-            crypto_data = pd.json_normalize(data['data'])
-
-            # Example: Extract and process data from the normalized DataFrame
-            # (Modify this part according to the JSON structure)
-            crypto_data['Timestamp'] = timestamp
-            crypto_data['Timestamp'] = pd.to_datetime(crypto_data['Timestamp'])
-
-            # Load the existing data from the CSV file (if it exists)
-            csv_filename = 'E:\\projects\\crypto_api\\api_project.csv'
-            try:
-                existing_data = pd.read_csv(csv_filename)
-            except FileNotFoundError:
-                existing_data = pd.DataFrame()
-
-            # Concatenate the new data with the existing data (if any)
-            updated_data = pd.concat([existing_data, crypto_data])
-
-            # Save the updated data to the CSV file
-            updated_data.to_csv(csv_filename, index=False)
-
-            print(f"Data updated and saved to {csv_filename}")
+        if response.status_code == 200:
+            data = response.json()
+            return data
         else:
-            print("No data to process.")
-    else:
-        print(f"Failed to retrieve data. Status code: {response.status_code}")
+            print(f"Failed to retrieve data. Status code: {response.status_code}")
+            return None
+    except requests.RequestException as e:
+        print(f"An error occurred while making the API request: {e}")
+        return None
 
-    #print(crypto_data.head(4))
+def update_csv(data, csv_filename):
+    if data:
+        timestamp = datetime.datetime.now()
+        crypto_data = pd.json_normalize(data['data'])
+        
+        # Convert the 'Timestamp' column to datetime type
+        crypto_data['Timestamp'] = pd.to_datetime(timestamp)
+        
+        try:
+            existing_data = pd.read_csv(csv_filename)
+        except FileNotFoundError:
+            existing_data = pd.DataFrame()
+
+        updated_data = pd.concat([existing_data, crypto_data])
+        updated_data.to_csv(csv_filename, index=False)
+        print(f"Data updated and saved to {csv_filename}")
+    else:
+        print("No data to process")
+
+if __name__ == "__main__":
+    crypto_data = get_crypto_data(API_URL, PARAMS, API_KEY)
+    if crypto_data:
+        update_csv(crypto_data, 'api_project.csv')
