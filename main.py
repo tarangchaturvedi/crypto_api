d38b573e7fcbe2ae4d73c76572fe6dc057081688
tarangchaturvedi <tarang.chaturvedi11@gmail.com>
2023-11-02 07:53:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from time import sleep
-from crypto_proj import api_runner
+import api_runner_func
 
 update_interval = 2
 no_itr = 5
 
+def main():
+    crypto_data = get_crypto_data(API_URL, PARAMS, API_KEY)
+    if crypto_data:
+        for i in range(no_itr):
+            update_csv(crypto_data, 'api_project.csv')
+            sleep(update_interval)
+
 if __name__ == "__main__":
-    for i in range(no_itr):
-        api_runner()
-        sleep(update_interval)
-    exit()
+    main()
